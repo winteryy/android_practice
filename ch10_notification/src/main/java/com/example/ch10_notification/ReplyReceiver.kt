f903f8a4ac6d58afbb22218d27ce2ae4b9e5f194
winteryy <lhk9805@gmail.com>
2022-07-22 04:09:51 +0900

    notification

---
+++ ch10_notification/src/main/java/com/example/ch10_notification/ReplyReceiver.kt
@@ -0,0 +1,21 @@
+package com.example.ch10_notification
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.RemoteInput
+
+class ReplyReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val replyText = RemoteInput.getResultsFromIntent(intent)
+                ?.getCharSequence("key_text_reply")
+        Log.d("kkang", "replyTxt : $replyText")
+
+        val manager = context.getSystemService(
+                AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(11)
+    }
+}
