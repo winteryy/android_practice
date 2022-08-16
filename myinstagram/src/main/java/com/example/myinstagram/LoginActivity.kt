190e92c289077ff891756a873bb8320eaa7c5d1e
winteryy <lhk9805@gmail.com>
2022-08-17 03:16:34 +0900

    add signUp system

--- myinstagram/src/main/java/com/example/myinstagram/LoginActivity.kt
+++ myinstagram/src/main/java/com/example/myinstagram/LoginActivity.kt
@@ -1,11 +1,68 @@
 package com.example.myinstagram
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.TextWatcher
+import android.text.style.ForegroundColorSpan
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myinstagram.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        //텍스트 색 처리
+        val ssb = SpannableStringBuilder(binding.registerBtn.text)
+        ssb.setSpan(ForegroundColorSpan(resources.getColor(R.color.lapis)),
+        11, 15, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.registerBtn.text = ssb
+
+        //로그인 구현부
+        var idCheck: Int = 0
+        var pwCheck: Int = 0
+
+        binding.userId.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(editable: Editable) {
+                idCheck = editable.length
+                binding.loginBtn.isEnabled = idCheck>0 && pwCheck>0
+            }
+        })
+        binding.userPw.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(editable: Editable) {
+                pwCheck = editable.length
+                binding.loginBtn.isEnabled = idCheck>0 && pwCheck>0
+            }
+        })
+
+        binding.loginBtn.setOnClickListener {
+            val userId: String = binding.userId.text.toString()
+            val userPw: String = binding.userPw.text.toString()
+            Log.d("ID", userId)
+            Log.d("PW", userPw)
+        }
+
+        //회원가입
+        binding.registerBtn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
+
 }
---
+++ myinstagram/src/main/java/com/example/myinstagram/RegisterActivity.kt
@@ -0,0 +1,65 @@
+package com.example.myinstagram
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myinstagram.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+    var auth : FirebaseAuth ?= null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        //글자수 체크
+        var idCheck: Int = 0
+        var pwCheck: Int = 0
+
+        binding.userId.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(editable: Editable) {
+                idCheck = editable.length
+                binding.signUpBtn.isEnabled = idCheck>0 && pwCheck>0
+            }
+        })
+        binding.userPw.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(editable: Editable) {
+                pwCheck = editable.length
+                binding.signUpBtn.isEnabled = idCheck>0 && pwCheck>0
+            }
+        })
+
+        //회원가입
+        binding.signUpBtn.setOnClickListener {
+            val email:String = binding.userId.text.toString()
+            val password:String = binding.userPw.text.toString()
+
+            auth?.createUserWithEmailAndPassword(email, password)?.addOnCompleteListener {
+                task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "가입 성공",
+                            Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, LoginActivity::class.java))
+                        finish()
+                    }else{
+                        Toast.makeText(baseContext, "중복되거나 유효하지 않은 입력입니다.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+            }
+
+        }
+    }
+}
